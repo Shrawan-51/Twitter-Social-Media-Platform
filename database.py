b4f3c1e79988638cb90fa957968b91e306e1341e
Shr1 <work.shrawan51@gmail.com>
2026-05-20 23:54:42 +0530

    Sql model setup

---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+SQLALCHEMY_DB_URL="sqlLite:///./blog.db"
+
+engine=create_engine(
+    SQLALCHEMY_DB_URL,
+    connect_args={"check_same_thread":False},
+)
+
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    with SessionLocal() as db:
+        yield db
